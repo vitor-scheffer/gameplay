45a3ae8a52fc55ab41f82db09e574ad1816fa7a0
Vitor Scheffer <vitorscheffer61@gmail.com>
2024-03-08 14:32:36 -0300

    feat: Implementa background

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,22 +2,28 @@ import { View, Image, Text } from "react-native";
 
 import { styles } from "./styles";
 import IllustrationImg from "../../assets/illustration.png";
+import { Background } from "../../components/Background";
 
 export function SignIn() {
   return (
-    <View style={styles.container}>
-      <Image source={IllustrationImg} style={styles.image} />
-      <View style={styles.content}>
-        <Text style={styles.title}>
-            Conecte-se {`\n`}
-            e organize suas {`\n`}
+    <Background>
+      <View style={styles.container}>
+        <Image
+          source={IllustrationImg}
+          style={styles.image}
+          resizeMode="stretch"
+        />
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Conecte-se {`\n`}e organize suas {`\n`}
             jogatinas
-        </Text>
-        <Text style={styles.subtitle}>
-        Crie grupos para jogar seus games {`\n`}
-        favoritos com seus amigos
-        </Text>
+          </Text>
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {`\n`}
+            favoritos com seus amigos
+          </Text>
+        </View>
       </View>
-    </View>
+    </Background>
   );
 }
